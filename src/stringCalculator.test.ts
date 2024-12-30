b219021e577ace4d145c09a0cdf924d5d4385581
conmecto <conmecto@gmail.com>
2024-12-30 15:09:06 +0530

    feat: test to check for single number

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -10,4 +10,8 @@ describe('StringCalculator', () => {
     test('should return 0 for an empty string', () => {
         expect(calculator.add('')).toBe(0);
     });
+
+    test('single number returns the value', () => {
+        expect(calculator.add('1')).toBe(1);
+    });
 });
