ad362fadb2a7026e1ee14143e17b0cdfa063c58a
conmecto <conmecto@gmail.com>
2024-12-30 15:46:46 +0530

    fix: supported sum for newline delimiter

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,11 +1,24 @@
 class StringCalculator  {
+    private static readonly ALLOWED_DELIMITERS = [',', '\n'];
 
-    add(numbers: string) {
+    add(numbers: string): number {
         if (!numbers) {
             return 0;
         }
-        const sum = numbers.split(',').reduce((sum: number, n: string) => sum + Number(n), 0);
+        const commaSplittedNumbers = this.splitNumbers(numbers, ',');
+        let sum = 0;
+        for(const partialString of commaSplittedNumbers) {
+            const newLineSplittedNumbers = this.splitNumbers(partialString, '\n');
+            sum += newLineSplittedNumbers.reduce((tempSum: number, n: string) => tempSum + Number(n), 0);
+        }
         return sum;
+    }
+
+    private splitNumbers(numbers: string, delimiter: string): string[] {
+        if (!StringCalculator.ALLOWED_DELIMITERS.includes(delimiter)) {
+            throw new Error('Delimiter not allowed');
+        }
+        return numbers.split(delimiter);
     }
     
 }
