c8afe11647292be32dc35aebf0955bcc59870c2e
conmecto <conmecto@gmail.com>
2024-12-30 16:34:54 +0530

    fix: supported custom delimiters

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,26 +1,39 @@
 class StringCalculator  {
-    private static readonly ALLOWED_DELIMITERS = [',', '\n'];
+    private static readonly CUSTOM_DELIMITER_START = '//'; 
 
     add(numbers: string): number {
         if (!numbers) {
             return 0;
         }
-        const commaSplittedNumbers = this.splitNumbers(numbers, ',');
-        let sum = 0;
-        for(const partialString of commaSplittedNumbers) {
-            const newLineSplittedNumbers = this.splitNumbers(partialString, '\n');
-            sum += newLineSplittedNumbers.reduce((tempSum: number, n: string) => tempSum + Number(n), 0);
+        const delimiters = [',', '\n'];
+        const parsedNumbersResponse = this.parseNumbers(numbers);
+        if (parsedNumbersResponse?.customDelimiter) {
+            delimiters.push(parsedNumbersResponse.customDelimiter);
+            numbers = parsedNumbersResponse.parsedNumbers;
         }
+        const splittedNumbers = this.splitNumbers(numbers, delimiters);
+        const sum = splittedNumbers.reduce((tempSum: number, n: string) => tempSum + Number(n), 0);
         return sum;
     }
 
-    private splitNumbers(numbers: string, delimiter: string): string[] {
-        if (!StringCalculator.ALLOWED_DELIMITERS.includes(delimiter)) {
-            throw new Error('Delimiter not allowed');
+    private parseNumbers(numbers: string): { customDelimiter: string, parsedNumbers: string } | undefined {
+        if (!numbers.startsWith(StringCalculator.CUSTOM_DELIMITER_START)) {
+            return undefined;
         }
-        return numbers.split(delimiter);
+        const delimiterEnd = numbers.indexOf('\n');
+        const customDelimiter = numbers.substring(2, delimiterEnd+1);
+        const parsedNumbers = numbers.substring(delimiterEnd+1);
+        return {
+            customDelimiter,
+            parsedNumbers
+        }
     }
-    
+
+    private splitNumbers(numbers: string, delimiters: string[]): string[] {
+        const regexString = delimiters.map(d => `$${d}`).join('')
+        const delimiterRegex = new RegExp(`[${regexString}]`);
+        return numbers.split(delimiterRegex);
+    }
 }
 
 export { StringCalculator }
